450fcd2f8ebda3497839c3e9866ade511eedc618
liang <lakupenda@hotmail.com>
2018-03-29 08:58:00 +0800

    MOD-增加文件下载功能

--- src/main/java/org/oneicy/controller/ExampleController.java
+++ src/main/java/org/oneicy/controller/ExampleController.java
@@ -8,11 +8,18 @@ import org.oneicy.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.Collection;
 
 @Controller
@@ -54,6 +61,21 @@ public class ExampleController {
 			logger.error("users: {}", organization.getUsers());
 		}
 		return list;
+	}
+
+	@RequestMapping("/org/export")
+	public ResponseEntity<byte[]> export() throws UnsupportedEncodingException {
+		HttpHeaders headers = new HttpHeaders();
+//		headers.setContentType(MediaType.parseMediaType("text/plain"));
+//		headers.set("Set-Cookie", "fileDownload=true;path=/");
+		// *MUST* do Set-Cookie in HTTP header, otherwise callback functions will not work in JS file
+		headers.setContentType(MediaType.TEXT_PLAIN);
+		headers.setContentDispositionFormData("attachment", URLEncoder.encode("文件名" + ".txt", "UTF-8"));
+		headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+		headers.setPragma("no-cache");
+
+		ResponseEntity<byte[]> response = new ResponseEntity<>(("aaaaaa颠三倒四龙卷风" + System.lineSeparator() + "fdafsd都结束了单发").getBytes(Charset.forName("UTF-8")), headers, HttpStatus.CREATED);
+		return response;
 	}
 
 }
