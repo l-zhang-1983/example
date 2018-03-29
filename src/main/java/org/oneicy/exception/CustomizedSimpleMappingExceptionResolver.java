13b24e682e884b245e3a477e1b56776b499a58ac
liang <lakupenda@hotmail.com>
2018-03-29 09:00:47 +0800

    MOD-优化异常信息显示 增加详细信息

--- src/main/java/org/oneicy/exception/CustomizedSimpleMappingExceptionResolver.java
+++ src/main/java/org/oneicy/exception/CustomizedSimpleMappingExceptionResolver.java
@@ -16,49 +16,43 @@ public class CustomizedSimpleMappingExceptionResolver extends SimpleMappingExcep
 	protected ModelAndView doResolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) {
 		// Expose ModelAndView for chosen error view.
 		String viewName = determineViewName(exception, request);
-		if (viewName != null) {
-			if (Utils.isAjaxRequest(request)) {
-				// ajax 请求 使用 JSON 返回
-				try {
-					response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-					response.setContentType("application/json");
-					PrintWriter writer = response.getWriter();
-					/*
-					 * JSONObject jo = new JSONObject(); jo.element("success",
-					 * false); jo.element("message", ex.getMessage());
-					 */
-					writer.write(((ExampleException) makeException(exception)).getExceptionMessage());
-					writer.flush();
-					writer.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				return null;
-			} else {
-				// JSP 返回
-				// 如果不是异步请求
-				// Apply HTTP status code for error views, if specified.
-				// Only apply it if we're processing a top-level request.
-				Integer statusCode = determineStatusCode(request, viewName);
-				if (statusCode != null) {
-					applyStatusCodeIfPossible(request, response, statusCode);
-				}
-
-				return getModelAndView(viewName, makeException(exception), request);
+		if (Utils.isAjaxRequest(request)) {
+			// ajax 请求 使用 JSON 返回
+			try {
+				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				response.setContentType("application/json");
+				PrintWriter writer = response.getWriter();
+				/*
+				 * JSONObject jo = new JSONObject(); jo.element("success",
+				 * false); jo.element("message", ex.getMessage());
+				 */
+				writer.write(((ExampleException) makeException(exception)).getExceptionMessage());
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} else {
 			return null;
+		} else {
+			// JSP 返回
+			// 如果不是异步请求
+			// Apply HTTP status code for error views, if specified.
+			// Only apply it if we're processing a top-level request.
+			Integer statusCode = determineStatusCode(request, viewName);
+			applyStatusCodeIfPossible(request, response, statusCode);
+
+			return getModelAndView(viewName, makeException(exception), request);
 		}
 	}
 
-	protected RuntimeException makeException(Exception exception) {
+	private RuntimeException makeException(Exception exception) {
 		ExampleException exampleException = new ExampleException();
 		if (exception instanceof UnauthenticatedException) {
 			exampleException.setExceptionMessage("没有权限访问吖");
 		} else if (exception instanceof  UnauthorizedException) {
 			exampleException.setExceptionMessage("还是没有权限访问吖");
 		} else {
-			exampleException.setExceptionMessage("出现异常");
+			exampleException.setExceptionMessage("出现异常: " + exception.getMessage());
 		}
 		return  exampleException;
 	}
